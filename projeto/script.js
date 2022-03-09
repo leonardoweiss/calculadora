0711b509afc9ec749ca29928855187367f2e93a5
Leonardo Weiss <lsweissleo@gmail.com>
2022-03-08 21:52:54 -0300

    Finished project

--- projeto/script.js
+++ projeto/script.js
@@ -1,4 +1,22 @@
-function inserir (num){
-	var num = 
-	document.getElementById('tela').value = num
+function inserir(num){
+	var numero = document.getElementById('tela').innerHTML;
+	document.getElementById('tela').innerHTML = numero + num;
+};
+
+function limparTudo(){
+	document.getElementById('tela').innerHTML = '';
+};
+
+function limparUm(){
+	var resultado = document.getElementById('tela').innerHTML;
+	document.getElementById('tela').innerHTML = resultado.substring(0, resultado.length -1);
 }
+
+function calcular(){
+	var resultado = document.getElementById('tela').innerHTML;
+	if (resultado){
+		document.getElementById('tela').innerHTML = eval(resultado);
+	}else{
+		document.getElementById('tela').innerHTML = 0;
+	};
+};
